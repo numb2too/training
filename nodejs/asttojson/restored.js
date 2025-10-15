67cd4f23d70a3bb483b31321404881e68191c03b
numb2too <milk518abc@gmail.com>
2025-10-15 18:01:36 +0800

    ast to json nodejs

---
+++ nodejs/asttojson/restored.js
@@ -0,0 +1,10 @@
+() => {
+  let d = [1856, 1824, 1776, 1728, 1776, 1728, 1776]
+  d = d.map((c) => String.fromCharCode(c >> 4));
+  console.log(d);
+}();
+/* 未知節點: FunctionDeclaration */
+/* 未知節點: FunctionDeclaration */
+let sensor = gen_sensor()
+console.log(sensor);
+/* 未知節點: jaajajajajajajajajajajajaj */
